f94a62a106dd00668b1cad0db11101a4e329fb81
code043 <procoder043@gmail.com>
2024-12-14 18:47:23 -0300

    Register route

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import multer from "multer";
 import cors from "cors";
 import { storage } from "./infra/multer/config";
+import CreateUser from "./application/usercases/user/CreateUser";
+import UserInMemory from "./infra/repositories/in-memory/user/UserInMemory";
 
 const app = express();
 app.use(cors());
@@ -9,7 +11,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const upload = multer({ storage });
-
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body
+  const bodyData = {
+    name,
+    email,
+    password
+  }
+  const create = new CreateUser(UserInMemory);
+  const data = await create.execute(bodyData);
+  res.status(201).json({
+    message: "Registered",
+    id: data
+  })
+})
 app.post("/photo", upload.single("image"), async (req, res) => {
   res.status(200).json({
     message: "ok",
