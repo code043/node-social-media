27237db4b49ac84e25c2fa89f65a81bfea1a63a9
code043 <procoder043@gmail.com>
2025-01-26 16:57:49 -0300

    Delete post usecase

--- src/application/usercases/post/DelePost.ts
+++ src/application/usercases/post/DelePost.ts
@@ -1,6 +1,6 @@
 import PostRepository from "../../repositories/post/PostRepository";
 
-export default class DelePost {
+export default class DeletePost {
   constructor(private repository: PostRepository) {}
   async execute(id: string): Promise<void> {
     const post = await this.repository.deletePost(id)
